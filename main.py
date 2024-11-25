56992a98474ddd2a17d35742bdfb5f9f4bb4c584
Germán Xander <german.xander@fio.unam.edu.ar>
2024-11-25 16:17:40 -0300

    dht22 en pin 15

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from machine import Pin
 import dht
 
-d = dht.DHT22(Pin(16))
+d = dht.DHT22(Pin(15))
 d.measure()
 temperatura=d.temperature()
-print(f"la temperatura actual es de {temperatura} C")
+print(f"\nla temperatura actual es de {temperatura} C")
 humedad=d.humidity()
 print(f"la humedad actual es de {humedad} %")

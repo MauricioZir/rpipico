331f145dc2ba22d114f5e6764789a587eae9c80c
MauricioZir <mauricionzir@gmail.com>
2025-03-20 15:15:28 -0300

    primer intento

--- main.py
+++ main.py
@@ -1,72 +1,15 @@
-# (C) Copyright Peter Hinch 2017-2019.
-# Released under the MIT licence.
-
-# This demo publishes to topic "result" and also subscribes to that topic.
-# This demonstrates bidirectional TLS communication.
-# You can also run the following on a PC to verify:
-# mosquitto_sub -h test.mosquitto.org -t result
-# To get mosquitto_sub to use a secure connection use this, offered by @gmrza:
-# mosquitto_sub -h <my local mosquitto server> -t result -u <username> -P <password> -p 8883
-
-# Public brokers https://github.com/mqtt/mqtt.github.io/wiki/public_brokers
-
-# red LED: ON == WiFi fail
-# green LED heartbeat: demonstrates scheduler is running.
-
-from mqtt_as import MQTTClient
-from mqtt_local import config
-import uasyncio as asyncio
-import dht, machine, json
-from collections import OrderedDict
-
-d = dht.DHT22(machine.Pin(13))
-
-def sub_cb(topic, msg, retained):
-    print('Topic = {} -> Valor = {}'.format(topic.decode(), msg.decode()))
-
-async def wifi_han(state):
-    print('Wifi is ', 'up' if state else 'down')
-    await asyncio.sleep(1)
-
-# If you connect with clean_session True, must re-subscribe (MQTT spec 3.1.2.4)
-async def conn_han(client):
-    await client.subscribe(config['client_id'], 1)
-
-async def main(client):
-    await client.connect()
-    n = 0
-    await asyncio.sleep(2)  # Give broker time
-    while True:
-        try:
-            d.measure()
-            try:
-                temperatura=d.temperature()
-                try:
-                    humedad=d.humidity()
-                    datos=json.dumps(OrderedDict([
-                        ('temperatura',temperatura),
-                        ('humedad',humedad)
-                    ]))
-                    await client.publish(config['client_id'], datos, qos = 1)
-                except OSError as e:
-                    print("sin sensor temperatura")
-            except OSError as e:
-                print("sin sensor humedad")
-        except OSError as e:
-            print("sin sensor")
-        await asyncio.sleep(180)  # Broker is slow
-
-# Define configuration
-config['subs_cb'] = sub_cb
-config['connect_coro'] = conn_han
-config['wifi_coro'] = wifi_han
-config['ssl'] = True
-
-# Set up client
-MQTTClient.DEBUG = True  # Optional
-client = MQTTClient(config)
-try:
-    asyncio.run(main(client))
-finally:
-    client.close()
-    asyncio.new_event_loop()
+from machine import Pin
+from time import sleep
+ 
+led_board = Pin("LED", Pin.OUT)
+sleep(1)    #le damos tiempo a vREPL
+print("\nLED esta destellando...")
+while True:
+    try:
+        led_board.toggle()
+        # led_board.value(not led_board.value())
+        sleep(.5) # sleep 1sec
+    except KeyboardInterrupt:
+        break
+led_board.off()
+print("Listo")

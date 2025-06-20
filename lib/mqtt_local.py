6d45529778bdad338c7d34993d3d598a2c7302f8
MauricioZir <mauricionzir@gmail.com>
2025-06-19 22:49:58 -0300

    V2

--- lib/mqtt_local.py
+++ lib/mqtt_local.py
@@ -1,7 +1,7 @@
 # mqtt_local.py Local configuration for mqtt_as demo programs.
 from sys import platform, implementation
 from mqtt_as import config
-from settings import SSID, password, BROKER, MQTT_USR, MQTT_PASS, MQTT_PORT
+from settings import SSID, password, BROKER
 
 config['server'] = BROKER  # Change to suit
 #  config['server'] = 'test.mosquitto.org'
